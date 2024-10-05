7401fe68720c1520fd563560814594d0225b7737
Redaboukir <Redaboy71@gmail.com>
2024-10-05 14:25:51 +0100

    edot

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 import Home from './components/Home/Home';
-import Whoweare from './components/Who-we-are/Whoweare';
-import Contact from './components/Contact/Contact';
 import './App.css';
 
 const App = () => {
@@ -12,10 +10,7 @@ const App = () => {
     switch (selectedComponent) {
       case 'home':
         return <Home />;
-      case 'Whoweare':
-        return <Whoweare />;
-      case 'contact':
-        return <Contact />;
+     
       default:
         return <Home />;
     }
